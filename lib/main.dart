2195cd5b1a9299a3340ee1407613cceb9e0ce721
mntm <ilspinoza@gmail.com>
2019-12-25 12:31:42 -0500

    asset-image

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-              image: NetworkImage("proto://url.to.the/image.png"),
+              image: AssetImage('images/diamond.png'),
             ),
         ),
       ),
