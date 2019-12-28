d1b2ac6460dfc4f29a3a77b6faad07b36a2c6ff7
mntm <ilspinoza@gmail.com>
2019-12-28 13:20:06 -0500

    I am poor challenge finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,31 @@
 import "package:flutter/material.dart";
 
-void main() {
-  runApp(
-    MaterialApp(
-      // The Scaffold widget permits us to add appbars 
+// With dart when a function (or method) contains only one instruction,
+// we can use the so called 'fat arrow' (=>) to shorten its declaration
+void main() => runApp(MyApp());
+
+// To be able to use hot reload and hot restart,
+// we need to declare the application as a Widget.
+// So that flutter could track the change made to the app
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // The Scaffold widget permits us to add appbars
       // and other material widget (FAB) in our application.
       // It is the equivalent of CoordinatorLayout on android.
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I am rich'),
-          backgroundColor: Colors.blueGrey[900],
+          title: Text('I am poor'),
+          backgroundColor: Colors.deepPurple[800],
         ),
         body: Center(
           child: Image(
-              // This feeds the widget with an image bundled with the app
-              image: AssetImage('images/diamond.png'),
-            ),
+            // This feeds the widget with an image bundled with the app
+            image: AssetImage('images/fb_poop_icon.png'),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
