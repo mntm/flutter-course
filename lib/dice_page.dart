8caf76783d5bab8a14f277be77ddb7c9bbcf1f8f
mntm <ilspinoza@gmail.com>
2019-12-29 10:17:37 -0500

    the 2 dices inside 1 button

--- lib/dice_page.dart
+++ lib/dice_page.dart
@@ -7,37 +7,40 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        leftDiceNumber = Random().nextInt(6) + 1;
-                      });
-                    },
-                    child: Image.asset('images/dice$leftDiceNumber.png'),
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        rightDiceNumber = Random().nextInt(6) + 1;
-                      });
-                    },
-                    child: Image.asset('images/dice$rightDiceNumber.png'),
-                  ),
-                ),
-              ],
+      child: FlatButton(
+        onPressed: () {
+          changeDiceFace();
+        },
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
             ),
-          );
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
