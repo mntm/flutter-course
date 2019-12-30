686208c887aa7442c32143d5db92cd92681225dd
mntm <ilspinoza@gmail.com>
2019-12-30 01:16:13 -0500

    Check if the user input right answer

--- lib/quizz_page.dart
+++ lib/quizz_page.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import "./question.dart";
+
 class Quizzler extends StatefulWidget {
   Quizzler({Key key}) : super(key: key);
 
@@ -8,22 +10,29 @@ class Quizzler extends StatefulWidget {
 }
 
 class _QuizzlerState extends State<Quizzler> {
-
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questions = [
+    Question(
+        mQuestion: 'You can lead a cow down stairs but not up stairs.',
+        mAnswer: "False"),
+    Question(
+        mQuestion: 'Approximately one quarter of human bones are in the feet.',
+        mAnswer: "True"),
+    Question(mQuestion: 'A slug\'s blood is green.', mAnswer: "True")
   ];
 
   int currentQuestion = 0;
 
-  void nextQuestion(){
-    setState((){
-      currentQuestion = (currentQuestion + 1) % 3;
-    });
+  void nextQuestion() {
+    print(currentQuestion);
+    if (currentQuestion < questions.length - 1) {
+      setState(() {
+        currentQuestion++;
+      });
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -31,59 +40,41 @@ class _QuizzlerState extends State<Quizzler> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          // The flex parameter ensures that this expanded widget 
+          // The flex parameter ensures that this expanded widget
           // takes 5 times more place than the others
-          flex : 5,
+          flex: 5,
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Center(
-              child: Text( questions[currentQuestion],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
+              child: questions[currentQuestion],
             ),
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-              color: Colors.green,
-              onPressed: () {
-                nextQuestion();
-              },
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-              color: Colors.red,
-              onPressed: () {
-                nextQuestion();
-              },
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
+        buildButton(color: Colors.green, text: 'True'),
+        buildButton(color: Colors.red, text: 'False'),
       ],
     );
+  }
+
+  Expanded buildButton({Color color,String text}) {
+    return Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FlatButton(
+            color: color,
+            onPressed: () {
+              print(questions[currentQuestion].isRightAnswer(text));
+              nextQuestion();
+            },
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+        ),
+      );
   }
 }
