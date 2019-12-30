1d981049c878baccc3b7978f72723a8b264846d8
mntm <ilspinoza@gmail.com>
2019-12-30 13:29:13 -0500

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import './story_brain.dart';
 
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -12,8 +11,6 @@ class Destini extends StatelessWidget {
     );
   }
 }
-
-
 
 StoryBrain storyBrain = StoryBrain();
 
@@ -29,9 +26,7 @@ class _StoryPageState extends State<StoryPage> {
         decoration: BoxDecoration(
           // Set a background image to a container
           image: DecorationImage(
-            image: AssetImage('images/background.png'),
-            fit: BoxFit.fill
-          ),
+              image: AssetImage('images/background.png'), fit: BoxFit.fill),
         ),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
@@ -42,7 +37,7 @@ class _StoryPageState extends State<StoryPage> {
               Expanded(
                 flex: 12,
                 child: Center(
-                  child: Text(                    
+                  child: Text(
                     storyBrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
@@ -54,8 +49,10 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 child: FlatButton(
                   onPressed: () {
-                    //Choice 1 made by user.                    
-                    storyBrain.nextStory(1);
+                    //Choice 1 made by user.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
                   },
                   color: Colors.red,
                   child: Text(
@@ -71,18 +68,21 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    storyBrain.nextStory(2);
-                  },
-                  color: Colors.blue,
-                  child: Text(
-                    storyBrain.getChoice2(),
-                    style: TextStyle(
-                      fontSize: 20.0,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: FlatButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    color: Colors.blue,
+                    child: Text(
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
@@ -94,7 +94,3 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
