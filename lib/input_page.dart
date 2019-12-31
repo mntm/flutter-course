a95ba8a215ea4e41c1c268455f31dde488932ac7
mntm <ilspinoza@gmail.com>
2019-12-31 07:25:15 -0500

    Added a container as bottom bar

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,9 @@
 import "package:flutter/material.dart";
 import "reusable_card.dart";
 
+const double bottomBarHeight = 56.0;
+const Color defaultBottomBarColor = Color(0xffeb1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -8,40 +11,46 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ReusableCard(),
-                ),
-                Expanded(
-                  child: ReusableCard(),
-                )
-              ],
-            ),
+  Widget build(BuildContext context) {    
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('BMI CALCULATOR'),
           ),
-          Expanded(
-            child: ReusableCard(),
-          ),
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ReusableCard(),
+          body: Column(
+            children: <Widget>[
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: ReusableCard(),
+                    ),
+                    Expanded(
+                      child: ReusableCard(),
+                    )
+                  ],
                 ),
-                Expanded(
-                  child: ReusableCard(),
-                )
-              ],
-            ),
-          )
+              ),
+              Expanded(
+                child: ReusableCard(),
+              ),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: ReusableCard(),
+                    ),
+                    Expanded(
+                      child: ReusableCard(),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: defaultBottomBarColor,
+                margin: EdgeInsets.only(top: 10.0),
+                width: double.infinity,
+                height: bottomBarHeight,
+          ),
         ],
       ),
     );
