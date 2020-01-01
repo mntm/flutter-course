d06ba6198486f3d2856c4b1e1845c39edc94f91c
mntm <ilspinoza@gmail.com>
2020-01-01 13:36:11 -0500

    Created 3rd cards

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,7 +5,6 @@ import "fa_icons.dart";
 import "icon_content.dart";
 import "constants.dart";
 
-
 enum Gender { MALE, FEMALE }
 
 class InputPage extends StatefulWidget {
@@ -15,6 +14,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
+  int height=180;
 
   @override
   Widget build(BuildContext context) {
@@ -22,64 +22,96 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ReusableCard(
-                    onTap: () {
-                      setState(() {
-                        selectedGender = Gender.MALE;
-                      });
-                    },
-                    color: (selectedGender == Gender.MALE)
-                        ? kAppActiveCardColor
-                        : kAppInactiveCardColor,
-                    child: IconContent(
-                        icon: FontAwesomeIcons.diamond, text: 'MALE'),
+      body: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(
+                      onTap: () {
+                        setState(() {
+                          selectedGender = Gender.MALE;
+                        });
+                      },
+                      color: (selectedGender == Gender.MALE)
+                          ? kAppActiveCardColor
+                          : kAppInactiveCardColor,
+                      child: IconContent(
+                          icon: FontAwesomeIcons.diamond, text: 'MALE'),
+                    ),
                   ),
+                  Expanded(
+                    child: ReusableCard(
+                      onTap: () {
+                        setState(() {
+                          selectedGender = Gender.FEMALE;
+                        });
+                      },
+                      color: (selectedGender == Gender.FEMALE)
+                          ? kAppActiveCardColor
+                          : kAppInactiveCardColor,
+                      child: IconContent(
+                          icon: FontAwesomeIcons.dollar, text: 'FEMALE'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'HEIGHT',
+                      style: kAppLabelTextStyle,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: <Widget>[
+                        Text(height.toString(), style: kAppNumberTextStyle),
+                        Text('cm', style: kAppLabelTextStyle)
+                      ],
+                    ),
+                    Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double val) {
+                        setState(() {
+                          height = val.round();
+                        }); 
+                      },
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: ReusableCard(
-                    onTap: () {
-                      setState(() {
-                        selectedGender = Gender.FEMALE;
-                      });
-                    },
-                    color: (selectedGender == Gender.FEMALE)
-                        ? kAppActiveCardColor
-                        : kAppInactiveCardColor,
-                    child: IconContent(
-                        icon: FontAwesomeIcons.dollar, text: 'FEMALE'),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(),
                   ),
-                )
-              ],
+                  Expanded(
+                    child: ReusableCard(),
+                  )
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ReusableCard(),
-          ),
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ReusableCard(),
-                ),
-                Expanded(
-                  child: ReusableCard(),
-                )
-              ],
+            Container(
+              color: kAppDefaultBottomBarColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomNavigationBarHeight,
             ),
-          ),
-          Container(
-            color: kAppDefaultBottomBarColor,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kAppBottomBarHeight,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
