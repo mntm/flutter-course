859dd883416b880d8ee0144153659d4b5a7eaad7
mntm <ilspinoza@gmail.com>
2020-01-01 11:18:28 -0500

    Custom FA Icons

---
+++ lib/fa_icons.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+class FontAwesomeIcons {
+   
+  static const IconData mars = const IconData(0xF06E, fontFamily: 'font-awesome');
+
+  static const IconData venus = const IconData(0xF221, fontFamily: 'font-awesome');
+
+  static const IconData diamond = const IconData(0xF3A5, fontFamily: 'font-awesome');
+  static const IconData dollar = const IconData(0xF3D1, fontFamily: 'font-awesome');
+}
