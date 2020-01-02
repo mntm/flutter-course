3306dd4087d03e911a6ec664f37b5337f7aa5557
mntm <ilspinoza@gmail.com>
2020-01-02 00:50:41 -0500

    added some boxed_plus and boxed_minus icon

--- lib/fa_icons.dart
+++ lib/fa_icons.dart
@@ -8,4 +8,7 @@ class FontAwesomeIcons {
 
   static const IconData diamond = const IconData(0xF3A5, fontFamily: 'font-awesome');
   static const IconData dollar = const IconData(0xF3D1, fontFamily: 'font-awesome');
+  static const IconData boxed_minus = const IconData(0xF146, fontFamily: 'font-awesome');
+  static const IconData boxed_plus = const IconData(0xF0FE, fontFamily: 'font-awesome');
+  
 }
